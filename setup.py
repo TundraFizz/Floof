7466683c3b665bfc804675efe0a46e4c8b5b1cad
Leif Coleman <mageleif@yahoo.com>
2016-08-30 12:52:25 -0500

    Remove useless files, add LICENSE, add .js libs, add unconverted images

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -24,7 +20,7 @@ tests_require = [
 setup(name='floof',
       version='0.0',
       description='floof',
-      long_description=README + '\n\n' + CHANGES,
+      long_description='floof',
       classifiers=[
           "Programming Language :: Python",
           "Framework :: Pyramid",
